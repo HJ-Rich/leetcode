0428a7cbb3904113d935ec593763a04cc354f516
Richard JEON <ztzy1907@gmail.com>
2022-02-22 01:53:49 +0900

    Time: 0 ms (100.00%), Space: 42.2 MB (6.20%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -19,33 +19,49 @@ class Solution {
     }
     
     public boolean search(TreeNode leftNode, TreeNode rightNode) {
-        if (leftNode == null && rightNode == null) { 
-            return true;            
+        if (leftNode == null && rightNode == null) {
+            return true;
         }
-        
+
         if (leftNode == null && rightNode != null || leftNode != null && rightNode == null) {
             return false;
         }
-        
-        
+
         if (leftNode.val != rightNode.val) {
             return false;
         }
 
-        boolean hasSameValue = leftNode.val == rightNode.val;
-        boolean noMoreChildOnLeft = leftNode.left == null && leftNode.right == null;
-        boolean noMoreChildOnRight = rightNode.left == null && rightNode.right == null;
-        if (noMoreChildOnLeft && noMoreChildOnRight && hasSameValue) {
+        if (hasSameValueWithoutAnyMoreChildren(leftNode, rightNode)) {
             return true;
         }
-        
-        if (leftNode.left == null && rightNode.left != null ||
-            leftNode.left != null && rightNode.left == null ||
-            leftNode.right == null && rightNode.right != null ||
-            leftNode.right != null && rightNode.right == null) {
+
+        if (hasDifferentNullChildren(leftNode, rightNode)) {
             return false;
         }
 
         return search(leftNode.left, rightNode.left) && search(leftNode.right, rightNode.right);
+    }
+
+    private boolean hasSameValueWithoutAnyMoreChildren(TreeNode leftNode, TreeNode rightNode) {
+        boolean hasSameValue = leftNode.val == rightNode.val;
+        boolean noMoreChildOnLeft = leftNode.left == null && leftNode.right == null;
+        boolean noMoreChildOnRight = rightNode.left == null && rightNode.right == null;
+        
+        if (noMoreChildOnLeft && noMoreChildOnRight && hasSameValue) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    private boolean hasDifferentNullChildren(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode.left == null && rightNode.left != null ||
+                leftNode.left != null && rightNode.left == null ||
+                leftNode.right == null && rightNode.right != null ||
+                leftNode.right != null && rightNode.right == null) {
+            return true;
+        }
+        
+        return false;
     }
 }
