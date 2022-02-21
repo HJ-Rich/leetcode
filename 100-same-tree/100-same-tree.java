16d5f462fc8c403ba174d661c2d7fa43f297d69c
Richard JEON <ztzy1907@gmail.com>
2022-02-22 01:49:56 +0900

    Time: 0 ms (100.00%), Space: 39.4 MB (37.48%) - LeetHub

---
+++ 100-same-tree/100-same-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return search(p, q);
+    }
+    
+    public boolean search(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) { 
+            return true;            
+        }
+        
+        if (leftNode == null && rightNode != null || leftNode != null && rightNode == null) {
+            return false;
+        }
+        
+        
+        if (leftNode.val != rightNode.val) {
+            return false;
+        }
+
+        boolean hasSameValue = leftNode.val == rightNode.val;
+        boolean noMoreChildOnLeft = leftNode.left == null && leftNode.right == null;
+        boolean noMoreChildOnRight = rightNode.left == null && rightNode.right == null;
+        if (noMoreChildOnLeft && noMoreChildOnRight && hasSameValue) {
+            return true;
+        }
+        
+        if (leftNode.left == null && rightNode.left != null ||
+            leftNode.left != null && rightNode.left == null ||
+            leftNode.right == null && rightNode.right != null ||
+            leftNode.right != null && rightNode.right == null) {
+            return false;
+        }
+
+        return search(leftNode.left, rightNode.left) && search(leftNode.right, rightNode.right);
+    }
+}
