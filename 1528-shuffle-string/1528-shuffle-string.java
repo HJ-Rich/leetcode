3e610ad89140fa455ff67a74e02ed9437fbdb16e
Richard JEON <ztzy1907@gmail.com>
2022-03-11 00:15:18 +0900

    Time: 10 ms (13.92%), Space: 46.8 MB (7.62%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+       String[] answerCharacters = new String[indices.length];
+        
+        String[] split = s.split("");
+        for (int i = 0; i < split.length; i++) {
+            answerCharacters[indices[i]] = split[i];
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (String answerCharacter : answerCharacters) {
+            answer.append(answerCharacter);
+        }
+        
+        return answer.toString(); 
+    }
+}
