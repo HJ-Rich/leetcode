01827bf5b7012687d2f2c4d20b112d4b0422774a
Richard JEON <ztzy1907@gmail.com>
2022-03-16 23:50:17 +0900

    Time: 4 ms (28.67%), Space: 42.3 MB (26.78%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,12 +1,8 @@
 class Solution {
     public String sortSentence(String s) {
         return Arrays.stream(s.split(" "))
-                    .sorted((s1, s2) -> {
-                        Integer s1Index = (int) s1.charAt(s1.length() - 1);
-                        Integer s2Index = (int) s2.charAt(s2.length() - 1);
-                        return s1Index.compareTo(s2Index);
-                    })
-                    .map(word -> word.substring(0, word.length() - 1))
-                    .collect(Collectors.joining(" "));
+                .sorted((s1, s2) -> Integer.compare(s1.charAt(s1.length() - 1), s2.charAt(s2.length() - 1)))
+                .map(word -> word.substring(0, word.length() - 1))
+                .collect(Collectors.joining(" "));
     }
 }
