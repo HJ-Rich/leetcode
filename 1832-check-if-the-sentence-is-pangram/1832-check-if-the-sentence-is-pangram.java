dd36d5c7df278d8efd2c5c3f0c59dc1689b0ee01
Richard JEON <ztzy1907@gmail.com>
2022-03-27 00:13:07 +0900

    Time: 4 ms (26.43%), Space: 42.5 MB (17.89%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        return IntStream.rangeClosed(97, 122)
+        return IntStream.rangeClosed('a', 'z')
                 .mapToObj(number -> String.valueOf((char) number))
                 .allMatch(sentence::contains);
     }
