97114ef86352f7e183373cca00b3df5cef0d4869
Richard JEON <ztzy1907@gmail.com>
2022-05-24 00:22:06 +0900

    Time: 769 ms (51.10%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,8 +1,5 @@
 SELECT u.name AS name,
-        CASE
-            WHEN SUM(r.distance) IS NULL THEN 0
-            ELSE SUM(r.distance)
-        END AS travelled_distance
+        IFNULL(SUM(r.distance), 0) AS travelled_distance
 FROM Users u
 LEFT OUTER JOIN Rides r ON u.id = r.user_id
 GROUP BY r.user_id
