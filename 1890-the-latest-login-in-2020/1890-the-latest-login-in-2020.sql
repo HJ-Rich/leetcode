a7dfd05f7ce0379bdab5f4cef91ea8bb478c8080
Richard JEON <ztzy1907@gmail.com>
2022-05-25 00:06:52 +0900

    Time: 719 ms (28.86%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,6 +1,5 @@
 SELECT user_id,
         MAX(time_stamp) AS last_stamp
 FROM Logins
-WHERE '2020-01-01' <= time_stamp
-    AND time_stamp < '2021-01-01'
+WHERE YEAR(time_stamp) = 2020
 GROUP BY user_id
