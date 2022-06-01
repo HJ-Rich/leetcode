1c87336bb515bce85fbd72e401a844a43f532d55
Richard JEON <ztzy1907@gmail.com>
2022-06-02 00:28:46 +0900

    Time: 3 ms (6.07%), Space: 41.6 MB (89.87%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        return Arrays.stream(accounts)
+                    .mapToInt(arr -> Arrays.stream(arr).sum())
+                    .max()
+                    .getAsInt();
+    }
+}
