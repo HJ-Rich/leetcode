ad16ebdba7c7a9bb62cd4b78b1797ac4fb00c2b6
Richard JEON <ztzy1907@gmail.com>
2022-10-06 14:16:01 +0900

    Time: 135 ms (11.25%), Space: 19.2 MB (10.88%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,9 @@
+from collections import deque
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # 알파벳 또는 숫자일 경우 담을 배열 선언
-        strs = []
+        strs = deque()
         # 문자열 s를 한 문자씩 순회
         for char in s:
             # isalnum() 은 알파벳 또는 숫자일 때 True
@@ -14,7 +16,7 @@ class Solution:
         while len(strs) > 1:
             # 배열 앞, 뒤에서 하나씩 꺼내서 다르면 False 얼리 리턴
             # pop() 은 기본값이 -1. 맨 뒤 요소가 반환되고 삭제됨. pop(0)은 맨 앞.
-            if strs.pop(0) != strs.pop():
+            if strs.popleft() != strs.pop():
                 return False
 
         return True
