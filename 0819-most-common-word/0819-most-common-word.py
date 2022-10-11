e80eac88161bf03db7198495e81abe1962251697
Richard JEON <ztzy1907@gmail.com>
2022-10-11 15:08:58 +0900

    Time: 46 ms (81.92%), Space: 13.9 MB (37.37%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,4 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+        return collections.Counter(words).most_common(1)[0][0]
